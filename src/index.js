df89efc41c35c956894c6537a3fd6feb90bdca4f
Aaron Suzuki <asuzuki125@gmail.com>
2016-11-03 11:18:30 -0400

    site near complete all responsive done and just need to finish content

--- src/index.js
+++ src/index.js
@@ -3,13 +3,54 @@ var $ = require('jquery');
 console.log("hello world")
 
 
-// $('.collapsed-menu').on('click', function () {
-
-// 		// $(this).parents('form').toggleClass('expand');
-// 		console.log("hi")
-
-// 	});
-
 $(".collapsed-menu").click(function(){
-    $(".main-navbar").toggle();
+    $(".collapsed-navbar").toggle();
+});
+
+
+$(".collapsed-navbar").click(function(){
+    $(".collapsed-navbar").toggle();
+});
+
+$(".back-button").click(function() {
+    window.history.back()
+});
+
+$('main').on('click', '.preform-button', function(){
+	location.href = "../form.html";
+	return false;
+});
+
+$('main').on('click', '.first-step-continue', function(){
+	$(this).parents('.application-first-step').toggle();
+	$(this).parents('.application-first-step').siblings('.application-second-step').toggle();
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-personal-info').toggleClass('active');
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-employment-info').toggleClass('active');
+	return false;
+});
+
+$('main').on('click', '.employment-back', function(){
+	$(this).parents('.application-second-step').toggle();
+	$(this).parents('.application-second-step').siblings('.application-first-step').toggle();
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-personal-info').toggleClass('active');
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-employment-info').toggleClass('active');
+})
+
+$('main').on('click', '.second-step-continue', function(){
+	$(this).parents('.application-second-step').toggle();
+	$(this).parents('.application-second-step').siblings('.application-third-step').toggle();
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-employment-info').toggleClass('active');
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-banking-info').toggleClass('active');
+	return false;
+});
+
+$('main').on('click', '.banking-back', function(){
+	$(this).parents('.application-third-step').toggle();
+	$(this).parents('.application-third-step').siblings('.application-second-step').toggle();
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-employment-info').toggleClass('active');
+	$(this).parents('.application-form').siblings('.application-progressbar').children('.bar-banking-info').toggleClass('active');
+});
+
+$('main').on('click', '.third-step-continue', function(){
+	return false;
 });
