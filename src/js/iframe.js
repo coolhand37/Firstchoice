144c996eb427f647a949c4aa8b7e28f1eabaaade
Christopher Bland <cjbland@gmail.com>
2016-11-23 09:10:02 -0500

    Adding the iframe source

---
+++ src/js/iframe.js
@@ -0,0 +1,104 @@
+$(function () {
+
+  var getParameterByName = function (name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  };
+
+  $("input[name='phone_home']").mask("(000) 000-0000");
+  $("input[name='phone_work']").mask("(000) 000-0000");
+  $("input[name='ssn']").mask("000-00-0000");
+
+  var tmp_amount = getParameterByName("amount");
+  if (tmp_amount == "") {
+    tmp_amount = "300";
+  }
+
+  var tmp_credit = getParameterByName("credit");
+  if (tmp_credit == "") {
+    tmp_credit = "0";
+  }
+
+  $("select[name='loan_amount_requested']").val(tmp_amount);
+  $("select[name='credit']").val(tmp_credit);
+  $("input[name='home_zipcode']").val(getParameterByName("zipcode"));
+
+  jQuery.validator.addMethod("phone", function (value, element) {
+    return this.optional(element) || /^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/.test(value);
+  }, "Valid format: (800) 555-1212");
+
+  jQuery.validator.addMethod("ssn", function (value, element) {
+    return this.optional(element) || /^[0-9]{3}-[0-9]{2}-[0-9]{4}$/.test(value);
+  }, "Valid format: 111-22-3333");
+
+  jQuery.validator.addMethod("phoneWork", function (value, element, param) {
+    return this.optional(element) || value != $(param).val();
+  }, "Cannot match home phone");
+
+  var form = $("#main-form");
+  form.validate({
+    errorClass: "error",
+    validClass: "success",
+    errorPlacement: function (error, element) {
+      error.appendTo(element.parents(".field"));
+    },
+    rules: {
+      email: { required: true, email: true },
+      phone_home: { required: true, phone: true },
+      phone_work: { required: true, phone: true, phoneWork: "input[name='phone_home']" },
+      ssn: { required: true, ssn: true },
+      bank_aba: {
+        required: true,
+        remote: {
+          url: "/helpers/validate/bankaba",
+          type: "GET",
+          dataType: "json",
+          data: {
+            aba: function () {
+              return $("input[name='bank_aba']").val();
+            }
+          }
+        }
+      }
+    }
+  });
+
+  $('main').on('click', '.first-step-continue', function() {
+    if (form.valid()) {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      $('.application-first-step').toggle();
+      $('.application-second-step').toggle();
+      $('.bar-personal-info').toggleClass('active');
+      $('.bar-employment-info').toggleClass('active');
+    }
+    return false;
+  });
+
+  $('main').on('click', '.employment-back', function() {
+    $('.application-second-step').toggle();
+    $('.application-first-step').toggle();
+    $('.bar-personal-info').toggleClass('active');
+    $('.bar-employment-info').toggleClass('active');
+  });
+
+  $('main').on('click', '.second-step-continue', function() {
+    if (form.valid()) {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      $('.application-second-step').toggle();
+      $('.application-third-step').toggle();
+      $('.bar-employment-info').toggleClass('active');
+      $('.bar-banking-info').toggleClass('active');
+    }
+    return false;
+  });
+
+  $('main').on('click', '.banking-back', function() {
+    $('.application-third-step').toggle();
+    $('.application-second-step').toggle();
+    $('.bar-employment-info').toggleClass('active');
+    $('.bar-banking-info').toggleClass('active');
+  });
+
+});
