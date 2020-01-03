119b2467338b45b75ee10380386b644941938896
Christopher Bland <cjbland@gmail.com>
2020-01-02 19:18:58 -0500

    Updating gulp to newer version

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,168 @@
-'use strict';
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const rev = require('gulp-rev');
+const useref = require('gulp-useref');
+const browserSync = require('browser-sync');
+const gulpif = require('gulp-if');
+const revRewrite = require('gulp-rev-rewrite');
+const awspublish = require("gulp-awspublish");
+const AWS = require("aws-sdk");
 
-var gulp = require('gulp');
-var rimraf = require('rimraf');
-var serve = require('gulp-serve');
-var uglify = require("gulp-uglify");
-var minifyCss = require("gulp-minify-css");
-var usemin = require("gulp-usemin");
-var rev = require("gulp-rev");
-var foreach = require("gulp-foreach");
-var s3 = require("gulp-s3-deploy");
-var rename = require("gulp-rename");
+const paths = {
+  styles: {
+    src: 'src/css/**/*.css',
+    dest: 'build/css/'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'build/js/'
+  },
+  html: {
+    src: 'src/**/*.html',
+    dest: 'build/'
+  },
+  images: {
+    src: 'src/images/**/*.*',
+    dest: 'build/images/'
+  },
+  fonts: {
+    src: 'src/media/**/*.*',
+    dest: 'build/media/'
+  }
+};
 
+// Below are the S3 properties.
+const publisher = awspublish.create({
+  region: 'us-east-1',
+  params: {
+    Bucket: 'fcpersonalloans.com'
+  },
+  credentials: new AWS.SharedIniFileCredentials({ profile: 'offerannex' })
+});
+const cache_control = {
+  'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate, public'
+};
 
-/****************************************
-  JS
-*****************************************/
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`,
+  // for example if you are using del 2.0 or above, return its promise
+  return del([ 'build' ]);
+}
 
-gulp.task("clean", function (cb) {
-  return rimraf("build", cb);
+/*
+ * Define our tasks using plain functions
+ */
+function styles() {
+  return gulp.src(paths.styles.src)
+      .pipe(cleanCSS())
+      .pipe(rev())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src)
+      .pipe(babel())
+      .pipe(uglify())
+      .pipe(rev())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
+
+function html() {
+  return gulp.src(paths.html.src)
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('*.css', cleanCSS()))
+      .pipe(gulpif('*.js', rev()))
+      .pipe(gulpif('*.css', rev()))
+      .pipe(revRewrite())
+      .pipe(gulp.dest(paths.html.dest));
+}
+
+function images() {
+  return gulp.src(paths.images.src)
+      .pipe(gulp.dest(paths.images.dest));
+}
+
+function fonts() {
+  return gulp.src(paths.fonts.src)
+      .pipe(gulp.dest(paths.fonts.dest));
+}
+
+function serve(cb) {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    port: 4000,
+    notify: false,
+    open: false
+  }, cb);
+}
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.html = html;
+exports.images = images;
+exports.fonts = fonts;
+exports.watch = watch;
+exports.serve = serve;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+const build = gulp.series(clean, gulp.parallel(html, images, fonts));
+
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+gulp.task('build', build);
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task('default', build);
+
+const non_html_src = [
+  './build/**/*.css',
+  './build/**/*.eot',
+  './build/**/*.svg',
+  './build/**/*.ttf',
+  './build/**/*.woff',
+  './build/**/*.gif',
+  './build/**/*.jpg',
+  './build/**/*.png',
+  './build/**/*.js',
+  './build/**/*.json',
+];
+
+gulp.task('publish-assets', function() {
+  return gulp.src(non_html_src)
+      .pipe(publisher.publish())
+      // .pipe(publisher.sync())
+      .pipe(awspublish.reporter());
 });
 
-gulp.task("images", function (cb) {
-  return gulp.src("./src/images/*.*")
-             .pipe(foreach(function (stream, file) {
-               return stream.pipe(gulp.dest("./build/images"));
-             }));
+gulp.task('publish-html', function() {
+  return gulp.src('./build/*.html')
+      .pipe(publisher.publish(cache_control))
+      .pipe(awspublish.reporter());
 });
-
-gulp.task("fonts", function () {
-  return gulp.src("./bower_components/font-awesome/fonts/*.*")
-             .pipe(foreach(function (stream, file) {
-               return stream.pipe(gulp.dest("./build/fonts"));
-             }));
-});
-
-gulp.task("js", function () {
-  return gulp.src(["./src/js/forminit.js"])
-             .pipe(uglify())
-             .pipe(rev())
-             .pipe(rename({ suffix: ".min" }))
-             .pipe(gulp.dest("./build/js"));
-});
-
-gulp.task("build", ["images", "fonts", "js"], function () {
-  return gulp.src("./src/*.html")
-             .pipe(foreach(function (stream, file) {
-               return stream.pipe(usemin({
-                 css: [minifyCss(), rev()],
-                 js: [uglify(), rev()]
-               }))
-               .pipe(gulp.dest("./build"));
-             }));
-});
-
-gulp.task("deploy", function () {
-  return gulp.src("./build/**")
-             .pipe(s3({
-               "key": process.env.AWS_ACCESS_KEY_ID || "none",
-               "secret": process.env.AWS_SECRET_ACCESS_KEY || "none",
-               "bucket": "fcpersonalloans.com",
-               "region": "us-east-1"
-             }));
-});
-
-/****************************************
-  Servers (Web and API)
-*****************************************/
-
-// apiServer.use(jsonServer.defaults);
-// apiServer.use(router);
-
-// gulp.task('serve:api', function (cb) {
-//   apiServer.listen(3000, cb);
-// });
-
-gulp.task('serve', serve({
-  root: ['.'],
-  port: process.env.PORT || 9000
-}));
-
-gulp.task("serve-build", serve({
-  root: ["./build"],
-  port: process.env.PORT || 8000
-}));
-
-/****************************************
-  Watch
-*****************************************/
-
-gulp.task('watch', ['build'], function () {
-  return gulp.watch(['src/**/*.js'], ['build'])
-})
-
-// Default
-gulp.task('default', ['serve', 'watch']);
